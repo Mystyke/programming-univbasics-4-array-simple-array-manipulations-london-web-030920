b3fd18eb3dceb73136f38116f94f0ff73fe473b1
Roshan F <roshanf@virginmedia.com>
2020-02-13 14:48:53 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,51 @@
+def using_push(array,string)
+  array.push(string)
+end
+
+def using_unshift(array,string)
+  array.unshift(string)
+end
+def using_pop(array)
+  array.pop 
+end
+
+def pop_with_arg(array,arg)
+  array.pop(arg)
+end
+
+def using_shift(array)
+  array.shift 
+end
+
+def shift_with_args(array,-)
+  array.shift(arg)
+end
+
+def using_concat(array,string)
+  array.concat(string)
+end
+
+def using_insert(array,ele)
+  array.insert(ele)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array,s)
+  array.delete(s)
+end
+
+def using_delete_at(array,n)
+  array.delete(n)
+end
+
+
+  
+
+
